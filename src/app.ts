7d965380a7dcd3038a780237290f1da7bfa302bc
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-11-04 18:42:53 +0600

    Configure user interface and user model

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Application, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+import { IAuthProvider, IsActive, IUser, Role } from "./user.interface";
+
+const authProviderSchema = new Schema<IAuthProvider>({
+    provider: { type: String, required: true },
+    providerID: { type: String, required: true }
+}, { versionKey: false, _id: false })
+
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    role: {
+        type: String,
+        enum: Object.values(Role),
+        default: Role.USER
+    },
+    address: { type: String },
+    phone: { type: String },
+    picture: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    isActive: {
+        type: String,
+        enum: Object.values(IsActive),
+        default: IsActive.ACTIVE
+    },
+    isVerified: { type: Boolean, default: false },
+    auths: [authProviderSchema]
+}, {
+    versionKey: false,
+    timestamps: true
+})
+
+
+export const USER = model<IUser>("USER", userSchema)
