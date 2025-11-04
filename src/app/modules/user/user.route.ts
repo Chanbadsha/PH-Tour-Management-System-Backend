14941cf045e8043cda31d5dc221b384e1e218877
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-11-04 22:55:53 +0600

    Implement zod schema for create user and update user

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UserAuthController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 export const UserRoutes = Router()
 
-UserRoutes.post('/register', UserAuthController.createUser)
+UserRoutes.post('/register', validateRequest(createUserZodSchema), UserAuthController.createUser)
